6cdf6d5a3cd29b5fb84b1b12428e9b1155b213ba
Aniket <aniketupatel1@gmail.com>
2019-06-26 14:35:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+var dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
+    }
+}
+
+function moveDodgerRight() {
+    var rightNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(rightNumbers, 10);
+
+    if (left <= 350) {
+        console.log(rightNumbers)
+        dodger.style.left = `${left + 10}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight()
+        
+    }
+});
